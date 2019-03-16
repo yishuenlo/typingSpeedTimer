c1cedc04b9cd50c8794166259bc7882d8c0edcfe
Yishuen Lo <lo.yishuen@gmail.com>
2019-03-16 11:51:30 -0600

    refine code
    fix timer glitch and add reset function

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const TIMERDISPLAY = document.querySelector(".timer");
-const PROMPT = document.querySelector("#prompt-text")
-const INPUT = document.querySelector("#input-area")
+const PROMPT = document.querySelector("#prompt-txt");
+const INPUT = document.querySelector("#input-area");
 const RESETBTN = document.querySelector("button");
 
 // select text input area when browser is loaded
 window.onload = INPUT.select();
 
 var interval;
-var timer = [0,0]; //min, sec
+var timer = [0,0,0]; //min, sec
 
 function leadingZero(time){
   if(time <=9){
@@ -17,29 +17,62 @@ function leadingZero(time){
 }
 
 function runTimer(){
- let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]+1);//add 1 to secomd bc of interval lag of 1000ms
+ let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]);//add 1 to secomd bc of interval lag of 1000ms
  TIMERDISPLAY.innerHTML = currentTime;
- timer[1]++; //increment seconds as the timer runs
+ timer[2]++; //increment seconds as the timer runs
 
  //convert time and display
- timer[0] = Math.floor(timer[1]/60); //minute = 1 sec/100 ms * 1 min/60 sec
- timer[1] = Math.floor((timer[1]) - (timer[0]*60));
+ //onvert ms to minute. since interval is set to 100ms:
+ // 1ms(100)(1sec/1000ms)(1min/60sec)
+ timer[0] = Math.floor(timer[2]*100/1000/60);
+
+  //onvert ms to second. since interval is set to 100ms:
+  // 1ms(100)(1sec/100ms) - 1min(60sec/1min)
+ timer[1] = Math.floor((timer[2]*100/1000) - (timer[0]*60));
 }
 
 function startTimer(){
   let inputTxtLength = INPUT.value.length;
   if(inputTxtLength == 0){
-    interval = setInterval(runTimer, 1000);
+    interval = setInterval(runTimer, 100); //interval of 100ms
   }
 }
 
 function spellCheck(){
   let txtEntered = INPUT.value;
-  let promptTxtMatch = PROMPT.substring(0, textEntered.length);
+  let promptTxtMatch = PROMPT.innerHTML.substring(0, txtEntered.length);
+
+  //if text matches exactly - game ends
+  if(txtEntered == PROMPT.innerHTML){
+    // stops timer
+    clearInterval(interval);
+
+    //disable textarea so users can't edit the text
+    INPUT.disabled = true;
+
+    // change border display and text to green to signal game over
+    INPUT.style.borderColor = "#74EBD5";
+    INPUT.style.color = "#4BD5C7";
+  } else {
+    if(txtEntered == promptTxtMatch){
+      INPUT.style.borderColor = "#E0E0E0"; //grey
+    } else {
+      INPUT.style.borderColor = "#EC407A"; //magenta
+    }
+  }
 }
 
 function reset(){
+  //reset interval and timer value
+  clearInterval(interval);
+  interval = null;
+  timer = [0,0,0];
 
+  //reset display
+  INPUT.value = "";
+  TIMERDISPLAY.innerHTML = "00:00";
+  INPUT.disabled = false;
+  INPUT.style.borderColor = "#74EBD5";
 }
 
 
